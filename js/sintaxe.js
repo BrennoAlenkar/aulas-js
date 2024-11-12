67bf2f20631b08de325ec9ea2bcae28e09b49fa4
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-11-12 10:39:24 -0300

    Aprendemos sintaxe em JavaScript

---
+++ js/sintaxe.js
@@ -0,0 +1,26 @@
+// JavaScript é case-sensitive, No JS faz diferença entre maiúsculas e minúsculas.
+let nome = "Brenno";
+
+let Nome = 'Kaio';
+
+console.log(Nome);
+
+console.log(nome);
+
+let numeros1 = 10;
+
+let _numero2 = 30;
+
+// Usando o camelCase (primeira letra do segundo nome Maiúscula).
+let meuNome = 'Brenno Alencar';
+
+let idadeUsuario = 23;
+
+let emailVerdadeiro = 'brennoAlencar@.com'
+
+//Nas declaraçoes de função usar o camelCase.
+function dizerNomeCompleto() {
+    let nomeCompleto = "Brenno Eduardo Alencar de Paula";
+
+    console.log(nomeCompleto);
+}
