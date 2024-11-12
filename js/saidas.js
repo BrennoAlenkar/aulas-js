c96dbda0d09062af50ca26c08f9562045745ddec
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-11-12 08:53:41 -0300

    Tipois de saídas

---
+++ js/saidas.js
@@ -0,0 +1,11 @@
+let nome = "Brenno";
+
+ alert("Olá, " + nome + "!");
+
+ console.log(10 < 1);
+
+ document.write(10 >= 10);
+
+let p = document.querySelector('#teste');
+
+p.innerHTML = 'Meu nome é Brenno';
